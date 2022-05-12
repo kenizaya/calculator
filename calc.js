232848ea58e22a2dfe49d59bc42cf9442cbc04b6
kenizaya <harveylstark@gmail.com>
2022-05-12 20:50:37 +0530

    Implement operate()

---
+++ calc.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(o, a, b) {
+    if (o === "+") return add(a, b);
+    if (o === "-") return subtract(a, b);
+    if (o === "*") return multiply(a, b);
+    if (o === "/") return divide(a, b);
+}
+
+console.log(operate("+", 5, 7));
